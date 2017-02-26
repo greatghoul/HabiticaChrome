262d643be4501aa9d6e9b38daaec2bf66f10e4c5
Mario Qiu <greatghoul@g2w.local>
2017-02-26 23:34:56 +0800

    init chrome extension using yo generator

---
+++ app/components/PopupApp.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="app">
+    hello world
+  </div>
+</template>
+
+<script>
+  
+</script>
+
+<style>
+  
+</style>
---
+++ app/scripts/popup.js
@@ -0,0 +1,7 @@
+import Vue from 'vue';
+import PopupApp from '../components/PopupApp.vue';
+
+new Vue({
+  el: '#app',
+  render: h => h(PopupApp)
+})
