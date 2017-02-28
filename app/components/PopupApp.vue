4c071112948075e82df83cfa55433eae79fa8022
Mario Qiu <greatghoul@g2w.local>
2017-02-28 23:46:27 +0800

    auto authorize
    - show different panel depends on state
    - if not authorize, click button to open auth page and authorize

--- app/components/PopupApp.vue
+++ app/components/PopupApp.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <section v-if="auth">
+    <section v-if="state == 'authed'">
       <div class="message-list">
       <message
           v-for="message in messages"
@@ -12,8 +12,8 @@
     </section>
     <section v-else>
       <div class="auth">
-        <p>Not authorized</p>
-        <button>Authorize</button>
+        <p>{{ description }}</p>
+        <button @click="authorize" v-if="state == 'unauthed'">Authorize</button>
       </div>
     </section>
   </div>
@@ -24,12 +24,25 @@ import Message from './Message.vue';
 import InputBox from './InputBox.vue';
 import storage from 'chrome-storage-wrapper';
 
+const STATE_MAPPING = {
+  loading: 'Loading ...',
+  unauthed: 'Not authorized',
+  authing: 'Authorizing ...',
+  authed: '',
+  failed: 'Please login habitica first.'
+};
+
 export default {
   data() {
     return {
       messages: [],
-      auth: true
+      state: 'loading'
     };
+  },
+  computed: {
+    description() {
+      return STATE_MAPPING[this.state];
+    }
   },
   created() {
     this.fetchMessages();
@@ -38,12 +51,19 @@ export default {
     fetchMessages() {
       const url = 'https://habitica.com/api/v3/groups/party/chat';
       storage.get('auth').then((options) => {
-        fetch(url, { headers: options.auth })
-          .then(response => response.json())
-          .then(json => this.messages = json.data.reverse().slice(0, 10))
-          .then(() => this.auth = true)
-          .then(() => this.scrollToBottom())
-          .catch(() => this.auth = false);
+        if (options.auth) {
+          fetch(url, { headers: options.auth })
+            .then(response => response.json())
+            .then(json => this.messages = json.data.reverse().slice(0, 10))
+            .then(() => this.state = 'authed')
+            .then(() => this.scrollToBottom())
+            .catch(() => {
+              this.state = 'unauthed';
+              storage.remove('auth');
+            });  
+        } else {
+          this.state = 'unauthed';
+        }
       });
     },
     scrollToBottom() {
@@ -51,6 +71,30 @@ export default {
         const elem = this.$el.querySelector('.message-list');
         console.log(elem);
         elem.scrollTop = elem.scrollHeight;
+      });
+    },
+    authorize() {
+      this.state = 'authing';
+      const url = 'https://habitica.com/#/options/settings/api'
+      chrome.tabs.create({ url: url, active: false }, (tab) => {
+        const script = {
+          code: 'localStorage.getItem("habit-mobile-settings");',
+          runAt: 'document_end'
+        };
+        chrome.tabs.executeScript(tab.id, script, results => {
+          if (results && results[0]) {
+            chrome.tabs.remove(tab.id);
+            const json = JSON.parse(results[0]);
+            storage.set('auth', {
+              'x-api-user': json.auth.apiId,
+              'x-api-key': json.auth.apiToken
+            });
+            this.state = 'authed';
+            this.fetchMessages();
+          } else {
+            this.state = 'failed';
+          }
+        });
       });
     }
   },
---
+++ app/scripts/inject.js
@@ -0,0 +1,1 @@
+console.log(localStorage.getItem('habit-mobile-settings'));
