08359427106af44f47d973654aed298a74f60fa9
Mario Qiu <greatghoul@g2w.local>
2017-03-10 23:55:11 +0800

    add font-awesome font path

--- tasks/fonts.js
+++ tasks/fonts.js
@@ -4,7 +4,12 @@ import livereload from 'gulp-livereload';
 import args from './lib/args';
 
 gulp.task('fonts', () => {
-  return gulp.src('app/fonts/**/*.{woff,woff2,ttf,eot,svg}')
+  const fontPaths = [
+    'app/fonts/**/*.{woff,woff2,ttf,eot,svg}',
+    'node_modules/font-awesome/fonts/*.{woff,woff2,ttf,eot,svg}'
+  ];
+
+  return gulp.src(fontPaths)
     .pipe(gulp.dest(`dist/${args.vendor}/fonts`))
     .pipe(gulpif(args.watch, livereload()));
 });
